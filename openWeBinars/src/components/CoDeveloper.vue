fe62528dedc478ace7fa353684d3d80bf3df140b
rivasnaur <103941525+rivasnaur@users.noreply.github.com>
2022-05-09 22:08:25 +0200

    v1.1

---
+++ openWeBinars/src/components/CoDeveloper.vue
@@ -0,0 +1,66 @@
+
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: 'CoDeveloper',
+    props: {
+      avatar: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      login: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: false
+      },
+      location: {
+        type: String,
+        required: false
+      },
+      company: {
+        type: String,
+        required: false
+      },
+      repos: {
+        type: Number,
+        required: false
+      },
+      gists: {
+        type: Number,
+        required: false
+      }
+    },
+    compputed: {
+        metadata () {
+            let meta = ''
+
+            if(this.email) {
+                meta = '${meta} - ${this.email}'
+            }
+
+            if(this.location) {
+                meta = '${meta} - ${this.location}'
+            }
+
+            if(this.company) {
+                meta = '${meta} - ${this.company}'
+            }
+            return meta
+        }
+    }
+  }
+</script>
+
+<style lang='css'>
+
+</style>
---
+++ openWeBinars/src/components/CoDevelopers.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul class="developers">
+        <li class="developers__item">
+            <co-developer></co-developer>
+</template>
+
+<script>
+    export default {
+        name: 'CoDevelopers',
+        data () {
+            return {
+                users: [],
+                fullName() {
+                    return '$(this.name) $(this.surname)'
+                }
+            }
+        }
+    }
+</script>
+
+<style lang='css'>
+</style>
